dcc241d04c1fd8ded494eb76e6cb60ee2bb5a3c5
guido2288 <guido2288@gmail.com>
2025-03-06 22:37:17 -0300

    Form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Form from "./components/Form"
 import Navbar from "./components/Navbar"
 
 function App() {
@@ -6,7 +7,8 @@ function App() {
   return (
     <div className="max-w-[990px] mx-auto overflow-hidden ">
       <Navbar />
-      <h1 className="">Analyze your text in real-time.</h1>
+      <h1 className="text-neutral-900 text-[40px] font-bold max-w-[340px] leading-9 text-center mx-auto my-8 md:max-w-[510px] md:text-[64px] md:leading-14">Analyze your text in real-time.</h1>
+      <Form />
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import { assets } from '../assets/assets'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between  mt-5'>
+    <div className='flex justify-between mt-5 md:mx-8 mx-4 lg:mx-0'>
         <img src={assets.logo_light_theme} alt="logo" />
-        <div className='bg-neutral-100 w-[44px] h-[44px] relative cursor-pointer' >
+        <div className='bg-neutral-100 w-[44px] h-[44px] rounded-lg relative cursor-pointer' >
           <img src={assets.icon_moon} alt="icon" className='absolute top-[11px] left-[11px]'/>
         </div>
     </div>
